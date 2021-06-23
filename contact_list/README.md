3fb6cfa65cf99303641e0bc51f5e6f0959893622
SergiyStetsenko <celtik45@gmail.com>
2021-06-23 15:54:55 +0300

    hw_01

---
+++ contact_list/README.md
@@ -0,0 +1,5 @@
+action list: http://joxi.ru/n2YNdbzCe3760A ;
+action get : http://joxi.ru/EA48KWpHp3vyvA ;
+action add:  http://joxi.ru/52ajnbQckMgqqA ;
+action remove: http://joxi.ru/MAjPzQMtkW1O6m ;
+
---
+++ contact_list/index.js
@@ -0,0 +1,32 @@
+const functions = require('./contacts.js');
+const argv = require('yargs').argv
+
+
+function invokeAction({ action, id, name, phone, email }) {
+    switch (action) {
+        case 'list':
+            functions.listContacts();
+            break;
+        case 'get':
+            functions.getContactById(id);
+            break;
+        case 'add':
+            functions.addContacts(name, email, phone)
+            break;
+        case 'remove':
+            functions.removeContact(id);
+            break;
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+invokeAction(argv)
+
+
+
+
+
+
+
+
